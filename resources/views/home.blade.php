fa54368a39e4414cb7df1727f0265fb165513c09
April <aprilianiw479@gmail.com>
2025-09-23 10:38:19 +0700

    memperbaiki form pesanan dalam template pelanggan

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -463,146 +463,18 @@
     `
     {{-- Reservation --}}
     <div id="reservation" class="section">
-        <div class="bg-image" style="background-image:url({{ asset('tpt/img/background03.jpg') }})"></div>
+        <div class="bg-image"
+            style="background-image:url({{ asset('tpt/img/background03.jpg') }}); 
+            height:500px; 
+            background-size:cover; 
+            background-position:center;">
+        </div>
+
         <div class="container">
 
             <div class="row">
-                <!-- reservation form -->
-                <div class="col-md-10 col-md-offset-1 col-sm-12">
-
-                    <form action="{{ route('formpesanan.store') }}" method="POST" class="reserve-form row">
-                        @csrf
-
-                        <div class="section-header text-center">
-                            <h4 class="sub-title">Reservasi</h4>
-                            <h2 class="title white-text">Pesanan Anda</h2>
-                        </div>
-
-                        <!-- Nama -->
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="nama">Nama</label>
-                                <input type="text" id="nama" name="nama"
-                                    class="input @error('nama') is-invalid @enderror" value="{{ old('nama') }}">
-                                @error('nama')
-                                    <div class="invalid-feedback d-block">{{ $message }}</div>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <!-- Telepon -->
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="telp">No. Telepon</label>
-                                <input type="text" id="telp" name="telp"
-                                    class="input @error('telp') is-invalid @enderror" value="{{ old('telp') }}">
-                                @error('telp')
-                                    <div class="invalid-feedback d-block">{{ $message }}</div>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <!-- Email -->
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="email">Email</label>
-                                <input type="email" id="email" name="email"
-                                    class="input @error('email') is-invalid @enderror" value="{{ old('email') }}">
-                                @error('email')
-                                    <div class="invalid-feedback d-block">{{ $message }}</div>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <!-- Alamat -->
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="alamat">Alamat</label>
-                                <textarea id="alamat" name="alamat" class="input @error('alamat') is-invalid @enderror">{{ old('alamat') }}</textarea>
-                                @error('alamat')
-                                    <div class="invalid-feedback d-block">{{ $message }}</div>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <!-- Pesanan (checkbox menu) -->
-                        <div class="col-md-12">
-                            <div class="form-group">
-                                <label>Pesanan</label>
-                                <div class="row">
-                                    @foreach ($menu->groupBy('kategori') as $kategori => $items)
-                                        <div class="col-12 mb-2">
-                                            <strong>{{ ucfirst($kategori) }}</strong>
-                                        </div>
-                                        @foreach ($items as $item)
-                                            <div class="col-md-6">
-                                                <label>
-                                                    <input type="checkbox" name="menu[{{ $item->id }}][id]"
-                                                        class="menu-checkbox" value="{{ $item->id }}"
-                                                        data-harga="{{ $item->harga }}">
-                                                    {{ $item->nama }} - Rp
-                                                    {{ number_format($item->harga, 0, ',', '.') }}
-                                                </label>
-                                                <input type="number" name="menu[{{ $item->id }}][jumlah]"
-                                                    class="menu-jumlah" value="1" min="1" disabled
-                                                    style="width:60px; margin-left:5px;">
-                                            </div>
-                                        @endforeach
-                                    @endforeach
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Metode Pembayaran -->
-                        <div class="col-md-12">
-                            <div class="form-group">
-                                <label for="metode_pembayaran">Metode Pembayaran</label>
-                                <select id="metode_pembayaran" name="metode_pembayaran"
-                                    class="input @error('metode_pembayaran') is-invalid @enderror">
-                                    <option value="">-- Pilih Metode --</option>
-                                    <option value="Transfer"
-                                        {{ old('metode_pembayaran') == 'Transfer' ? 'selected' : '' }}>
-                                        Transfer Bank</option>
-                                    <option value="QRIS" {{ old('metode_pembayaran') == 'QRIS' ? 'selected' : '' }}>QRIS
-                                    </option>
-                                </select>
-                                @error('metode_pembayaran')
-                                    <div class="invalid-feedback d-block">{{ $message }}</div>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <!-- Catatan -->
-                        <div class="col-md-12">
-                            <div class="form-group">
-                                <label for="catatan">Pesan untuk Penjual</label>
-                                <textarea id="catatan" name="catatan" class="input @error('catatan') is-invalid @enderror">{{ old('catatan') }}</textarea>
-                                @error('catatan')
-                                    <div class="invalid-feedback d-block">{{ $message }}</div>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <!-- Total harga -->
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="total_harga">Total Harga</label>
-                                <input type="number" id="total_harga" name="total_harga" class="input"
-                                    value="{{ old('total_harga') }}" readonly>
-                            </div>
-                        </div>
-
-                        <!-- Tombol -->
-                        <div class="col-md-12 text-center">
-                            <button type="submit" class="main-button">Pesan Sekarang</button>
-                        </div>
-                    </form>
-
-                </div>
-                <!-- /reservation form -->
-
                 <!-- opening time -->
-                <div class="col-md-4 col-md-offset-0 col-sm-10 col-sm-offset-1">
+                <div class="col-md-6 col-sm-10 col-md-offset-3 col-sm-offset-1">
                     <div class="opening-time row">
                         <div class="section-header text-center">
                             <h2 class="title white-text">Jam Buka</h2>
--- resources/views/layouts/guest/navbar.blade.php
+++ resources/views/layouts/guest/navbar.blade.php
@@ -33,13 +33,12 @@
                      <li><a href="#home">Home</a></li>
                      <li><a href="#about">About</a></li>
                      <li><a href="#menu">Menu</a></li>
-                     <li><a href="#reservation">Pesan</a></li>
                  </ul>
                  <!-- /nav -->
 
                  <!-- button nav -->
                  <ul class="cta-nav">
-                     <li><a href="#reservation" class="main-button">Pesan</a></li>
+                     <li><a href="{{ route('pesanan') }}" class="main-button">Pesan</a></li>
                  </ul>
                  <!-- button nav -->
 
