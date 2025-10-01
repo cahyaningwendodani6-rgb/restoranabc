e42a8818ecdce3a009a9185c5966a0b753b05f29
April <aprilianiw479@gmail.com>
2025-10-01 10:14:08 +0700

    memperbaiki navbar

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE HTML>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -22,7 +23,7 @@
     <link rel="stylesheet" href="{{ asset('tpt/css/flexslider.css') }}">
     <link rel="stylesheet" href="{{ asset('tpt/css/style.css') }}">
 
-    
+
 
     {{-- Modernizr --}}
     <script src="{{ asset('tpt/js/modernizr-2.6.2.min.js') }}"></script>
@@ -30,109 +31,123 @@
     <script src="{{ asset('tpt/js/respond.min.js') }}"></script>
     <![endif]-->
 </head>
+
 <body>
 
-<div class="fh5co-loader"></div>
+    <div class="fh5co-loader"></div>
 
-<div id="page">
-    {{-- Navbar --}}
-    <nav class="fh5co-nav" role="navigation">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12 text-center logo-wrap">
-                    <div id="fh5co-logo">
-                        <a href="{{ url('/') }}">RestoranABC<span>.</span></a>
+    <div id="page">
+        {{-- Navbar --}}
+        <nav class="fh5co-nav" role="navigation">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 text-center logo-wrap">
+                        <div id="fh5co-logo">
+                            <a href="{{ url('/') }}">RestoranABC<span>.</span></a>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 text-center menu-1 menu-wrap">
+                        <ul>
+                            <li class="{{ request()->is('/') ? 'active' : '' }}">
+                                <a href="{{ url('/') }}">Home</a>
+                            </li>
+                            <li class="{{ request()->is('menunya') ? 'active' : '' }}">
+                                <a href="{{ url('/menunya') }}">Menu</a>
+                            </li>
+                            <li class="{{ request()->is('gallery*') ? 'active' : '' }}">
+                                <a href="{{ url('/gallery') }}">Gallery</a>
+                            </li>
+                            <li class="{{ request()->is('reservation*') ? 'active' : '' }}">
+                                <a href="{{ url('/reservation') }}">Reservation</a>
+                            </li>
+                            <li class="{{ request()->is('about') ? 'active' : '' }}">
+                                <a href="{{ url('/about') }}">About</a>
+                            </li>
+                            <li class="{{ request()->is('contact') ? 'active' : '' }}">
+                                <a href="{{ url('/contact') }}">Contact</a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-                <div class="col-xs-12 text-center menu-1 menu-wrap">
-                    <ul>
-                        <li><a href="{{ url('/') }}">Home</a></li>
-                        <li><a href="{{ url('/menunya') }}">Menu</a></li>
-                        <li><a href="{{ url('/gallery') }}">Gallery</a></li>
-                        <li><a href="{{ url('/reservation') }}">Reservation</a></li>
-                        <li><a href="{{ url('/about') }}">About</a></li>
-                        <li><a href="{{ url('/contact') }}">Contact</a></li>
-                    </ul>
-                </div>
             </div>
-        </div>
-    </nav>
+        </nav>
 
-    {{-- Konten Dinamis --}}
-    @yield('content')
+        {{-- Konten Dinamis --}}
+        @yield('content')
 
-    {{-- Footer --}}
-    <footer id="fh5co-footer" role="contentinfo" class="fh5co-section">
-        <div class="container">
-            <div class="row row-pb-md">
-                <div class="col-md-4 fh5co-widget">
-                    <h4>Restoran ABC</h4>
-                    <p>Kami menyajikan makanan dan minuman terbaik dengan bahan segar pilihan setiap hari.</p>
+        {{-- Footer --}}
+        <footer id="fh5co-footer" role="contentinfo" class="fh5co-section">
+            <div class="container">
+                <div class="row row-pb-md">
+                    <div class="col-md-4 fh5co-widget">
+                        <h4>Restoran ABC</h4>
+                        <p>Kami menyajikan makanan dan minuman terbaik dengan bahan segar pilihan setiap hari.</p>
+                    </div>
+                    <div class="col-md-2 col-md-push-1 fh5co-widget">
+                        <h4>Links</h4>
+                        <ul class="fh5co-footer-links">
+                            <li><a href="{{ url('/') }}">Home</a></li>
+                            <li><a href="{{ url('/about') }}">About</a></li>
+                            <li><a href="{{ url('/menu') }}">Menu</a></li>
+                            <li><a href="{{ url('/gallery') }}">Gallery</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-2 col-md-push-1 fh5co-widget">
+                        <h4>Kategori</h4>
+                        <ul class="fh5co-footer-links">
+                            <li><a href="#">Makanan</a></li>
+                            <li><a href="#">Minuman</a></li>
+                            <li><a href="#">Cemilan</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4 col-md-push-1 fh5co-widget">
+                        <h4>Kontak</h4>
+                        <ul class="fh5co-footer-links">
+                            <li>Jl. Contoh No.123, Jakarta</li>
+                            <li><a href="tel://08123456789">+62 812-3456-789</a></li>
+                            <li><a href="mailto:info@restoranabc.com">info@restoranabc.com</a></li>
+                            <li><a href="http://restoranabc.com">restoranabc.com</a></li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="col-md-2 col-md-push-1 fh5co-widget">
-                    <h4>Links</h4>
-                    <ul class="fh5co-footer-links">
-                        <li><a href="{{ url('/') }}">Home</a></li>
-                        <li><a href="{{ url('/about') }}">About</a></li>
-                        <li><a href="{{ url('/menu') }}">Menu</a></li>
-                        <li><a href="{{ url('/gallery') }}">Gallery</a></li>
-                    </ul>
-                </div>
-                <div class="col-md-2 col-md-push-1 fh5co-widget">
-                    <h4>Kategori</h4>
-                    <ul class="fh5co-footer-links">
-                        <li><a href="#">Makanan</a></li>
-                        <li><a href="#">Minuman</a></li>
-                        <li><a href="#">Cemilan</a></li>
-                    </ul>
-                </div>
-                <div class="col-md-4 col-md-push-1 fh5co-widget">
-                    <h4>Kontak</h4>
-                    <ul class="fh5co-footer-links">
-                        <li>Jl. Contoh No.123, Jakarta</li>
-                        <li><a href="tel://08123456789">+62 812-3456-789</a></li>
-                        <li><a href="mailto:info@restoranabc.com">info@restoranabc.com</a></li>
-                        <li><a href="http://restoranabc.com">restoranabc.com</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="row copyright">
-                <div class="col-md-12 text-center">
-                    <p>
-                        <small class="block">&copy; {{ date('Y') }} Restoran ABC. All Rights Reserved.</small>
-                        <small class="block">Designed by FreeHTML5.co | Customized by Restoran ABC</small>
-                    </p>
-                    <p>
+                <div class="row copyright">
+                    <div class="col-md-12 text-center">
+                        <p>
+                            <small class="block">&copy; {{ date('Y') }} Restoran ABC. All Rights Reserved.</small>
+                            <small class="block">Designed by FreeHTML5.co | Customized by Restoran ABC</small>
+                        </p>
+                        <p>
                         <ul class="fh5co-social-icons">
                             <li><a href="#"><i class="icon-twitter2"></i></a></li>
                             <li><a href="#"><i class="icon-facebook2"></i></a></li>
                             <li><a href="#"><i class="icon-instagram"></i></a></li>
                         </ul>
-                    </p>
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
-    </footer>
-</div>
+        </footer>
+    </div>
 
-{{-- Scroll to top --}}
-<div class="gototop js-top">
-    <a href="#" class="js-gotop"><i class="icon-arrow-up22"></i></a>
-</div>
+    {{-- Scroll to top --}}
+    <div class="gototop js-top">
+        <a href="#" class="js-gotop"><i class="icon-arrow-up22"></i></a>
+    </div>
 
-{{-- JS --}}
-<script src="{{ asset('tpt/js/jquery.min.js') }}"></script>
-<script src="{{ asset('tpt/js/jquery.easing.1.3.js') }}"></script>
-<script src="{{ asset('tpt/js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('tpt/js/jquery.waypoints.min.js') }}"></script>
-<script src="{{ asset('tpt/js/jquery.stellar.min.js') }}"></script>
-<script src="{{ asset('tpt/js/jquery.flexslider-min.js') }}"></script>
-<script src="{{ asset('tpt/js/main.js') }}"></script>
+    {{-- JS --}}
+    <script src="{{ asset('tpt/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('tpt/js/jquery.easing.1.3.js') }}"></script>
+    <script src="{{ asset('tpt/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('tpt/js/jquery.waypoints.min.js') }}"></script>
+    <script src="{{ asset('tpt/js/jquery.stellar.min.js') }}"></script>
+    <script src="{{ asset('tpt/js/jquery.flexslider-min.js') }}"></script>
+    <script src="{{ asset('tpt/js/main.js') }}"></script>
 
-@yield('scripts')
-@stack('scripts')
+    @yield('scripts')
+    @stack('scripts')
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
 </body>
+
 </html>
